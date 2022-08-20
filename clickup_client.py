ca6677f3ac3770321bb1b4a9f6fad4e764dd95ed
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-08-21 04:01:25 +0900

    :zap: made script fetching all task

--- clickup_client.py
+++ clickup_client.py
@@ -16,6 +16,51 @@ class ClickUpClient():
     def set_post_per_min(self, post_per_min):
         self.post_per_min = post_per_min
         self.interval_micros = 1000000.0 * 60.0 / self.post_per_min
+        
+    # FOLDER
+    def get_folders(self, space_id, params=dict()):
+        headers = {
+            "Authorization": self.auth,
+            "Content-Type": "application/json"
+        }
+        url = f"https://api.clickup.com/api/v2/space/{space_id}/folder"
+
+        resp = self.access("GET", url, {
+            "timeout": self.TIMEOUT_SEC,
+            "headers": headers,
+            "params": params
+        })
+        return resp
+
+    # LIST
+    def get_folderless_list(self, space_id, params=dict()):
+        headers = {
+            "Authorization": self.auth,
+            "Content-Type": "application/json"
+        }
+        url = f"https://api.clickup.com/api/v2/space/{space_id}/list"
+
+        resp = self.access("GET", url, {
+            "timeout": self.TIMEOUT_SEC,
+            "headers": headers,
+            "params": params
+        })
+        return resp
+
+    # TASK
+    def get_tasks(self, list_id, params=dict()):
+        headers = {
+            "Authorization": self.auth,
+            "Content-Type": "application/json"
+        }
+        url = f"https://api.clickup.com/api/v2/list/{list_id}/task"
+
+        resp = self.access("GET", url, {
+            "timeout": self.TIMEOUT_SEC,
+            "headers": headers,
+            "params": params
+        })
+        return resp
 
     def create_task_template(self, body, list_id, template_id):
         headers = {
@@ -31,19 +76,6 @@ class ClickUpClient():
         })
         return resp
 
-    def get_folderless_list(self, space_id, params):
-        headers = {
-            "Authorization": self.auth,
-            "Content-Type": "application/json"
-        }
-        url = f"https://api.clickup.com/api/v2/space/{space_id}/list"
-
-        resp = self.access("GET", url, {
-            "timeout": self.TIMEOUT_SEC,
-            "headers": headers,
-            "params": params
-        })
-        return resp
 
     def access(self, method, url, args):
         PRINT_MICROS_THRESHOLD = 3000000
@@ -51,7 +83,7 @@ class ClickUpClient():
         ).microsecond - self.last_accessed_time
         if elapsed_micros < self.interval_micros:
             # wait if it is too less time from last access
-            wait_micros = self.interval_mircos - elapsed_micros
+            wait_micros = self.interval_micros - elapsed_micros
             if wait_micros > PRINT_MICROS_THRESHOLD:
                 print(f"waiting {wait_micros} microsec")
             wait_sec = wait_micros / 1000000.0
---
+++ get_all_task.py
@@ -0,0 +1,76 @@
+import os
+from os.path import dirname, join
+
+from clickup_client import *
+
+from dotenv import load_dotenv
+
+ENV_AUTH = "CLICKUP_AUTH"
+ENV_SPACE = "CLICKUP_SPACE"
+
+# maximum API run in a minute
+POST_PER_MIN = 60
+
+def get_all_lists(client, space_id, params=dict()):
+    lists = []
+
+    resp = c.get_folderless_list(space_id, params)
+    lists += resp.json()["lists"]
+
+    resp = c.get_folders(space_id, params)
+    folder = resp.json()["folders"]
+    lists += sum([f["lists"] for f in folder], [])
+
+    return lists
+
+
+def get_all_tasks_in_list(client, list_id, params=dict()):
+    tasks = []
+    page = 0
+    params["subtasks"] = True
+
+    while True:
+        params["page"] = page
+        resp = client.get_tasks(list_id, params)
+        if len(resp.json()["tasks"]) == 0:
+            break
+
+        tasks += resp.json()["tasks"]
+        if len(resp.json()["tasks"]) != 100:
+            break
+
+        page += 1
+
+    return tasks
+
+
+def get_all_tasks(client, space_id, list_param=dict(), task_param=dict()):
+    lists = get_all_lists(client, space_id, list_param)
+
+    tasks = []
+    for l in lists:
+        tasks += get_all_tasks_in_list(client, l["id"], task_param)
+
+    return tasks
+
+
+if __name__ == "__main__":
+    # load dotenv
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(dotenv_path)
+
+    auth = os.environ.get(ENV_AUTH)
+    space_id = os.environ.get(ENV_SPACE)
+
+    c = ClickUpClient(auth, POST_PER_MIN)
+
+    tasks = get_all_tasks(c,
+                          space_id,
+                          list_param={"archived": True},
+                          task_param={
+                              "archived": True,
+                              "order_by": "created",
+                              "subtasks": True,
+                              "include_closed": True,
+                          })
+    print("\n".join(map(str, tasks)))
