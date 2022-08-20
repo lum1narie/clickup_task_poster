475ccc9e2f5308b252ed6217d8f392ce90eb6347
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-08-21 05:23:06 +0900

    :recycle: split utility code and all task fetching code

--- get_all_task.py
+++ get_all_task.py
@@ -1,7 +1,9 @@
 import os
 from os.path import dirname, join
+import datetime
 
 from clickup_client import *
+from clickup_utility import *
 
 from dotenv import load_dotenv
 
@@ -10,51 +12,6 @@ ENV_SPACE = "CLICKUP_SPACE"
 
 # maximum API run in a minute
 POST_PER_MIN = 60
-
-
-def get_all_lists(client, space_id, params=dict()):
-    lists = []
-
-    resp = c.get_folderless_list(space_id, params)
-    lists += resp.json()["lists"]
-
-    resp = c.get_folders(space_id, params)
-    folder = resp.json()["folders"]
-    lists += sum([f["lists"] for f in folder], [])
-
-    return lists
-
-
-def get_all_tasks_in_list(client, list_id, params=dict()):
-    tasks = []
-    page = 0
-    params["subtasks"] = True
-
-    while True:
-        params["page"] = page
-        resp = client.get_tasks(list_id, params)
-        if "tasks" not in resp.json():
-            print(resp.json())
-            break
-
-        tasks += resp.json()["tasks"]
-        if len(resp.json()["tasks"]) != 100:
-            break
-
-        page += 1
-
-    return tasks
-
-
-def get_all_tasks(client, space_id, list_param=dict(), task_param=dict()):
-    lists = get_all_lists(client, space_id, list_param)
-
-    tasks = []
-    for l in lists:
-        tasks += get_all_tasks_in_list(client, l["id"], task_param)
-
-    return tasks
-
 
 if __name__ == "__main__":
     # load dotenv
@@ -66,13 +23,17 @@ if __name__ == "__main__":
 
     c = ClickUpClient(auth, POST_PER_MIN)
 
+    d = int(datetime.datetime.strptime('20220801',
+                                       '%Y%m%d').timestamp()) * 1000
+    task_param = {
+        "archived": True,
+        "order_by": "created",
+        "subtasks": True,
+        "include_closed": True,
+        "date_created_gt": d
+    }
     tasks = get_all_tasks(c,
                           space_id,
                           list_param={"archived": True},
-                          task_param={
-                              "archived": True,
-                              "order_by": "created",
-                              "subtasks": True,
-                              "include_closed": True,
-                          })
+                          task_param=task_param)
     print("\n".join(map(str, tasks)))
